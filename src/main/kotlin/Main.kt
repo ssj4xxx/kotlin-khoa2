7d20b69696f1728cf32656bbaca935d355bd42e3
SSJ4 <varus552@gmail.com>
2022-11-01 16:44:50 +0700

    Learning

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,4 +4,8 @@ fun main(args: Array<String>) {
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
+    val greet = "hello"
+    println(greet)
+    println(greet::class)
+    println(greet.javaClass)
 }
