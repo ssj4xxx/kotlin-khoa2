44a0723d4850df33675a1d1f9ca8024e37056191
SSJ4 <varus552@gmail.com>
2022-11-06 13:51:13 +0700

    Learning kotlin

--- src/main/kotlin/test2.kts
+++ src/main/kotlin/test2.kts
@@ -1,7 +1,12 @@
-val sites = mapOf("pragprog" to "https://www.pragprog.com",
-    "agiledeveloper" to "https://agiledeveloper.com")
-println(sites.size)
-println(sites["pragprog"])
-for (entry in sites) {
-    println("${entry.key} --- ${entry.value}")
+fun nickName(name: String?): String? {
+    if (name == "William") {
+        return "Bill"
+    }
+    if (name != null) {
+        return name.reversed()
+    }
+    return null
 }
+println("Nickname for William is ${nickName("William")}")
+println("Nickname for Venkat is ${nickName("Venkat")}")
+println("Nickname for null is ${nickName(null)}")
