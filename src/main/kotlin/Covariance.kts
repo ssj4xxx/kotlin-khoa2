179ead838bd836490e430f3ab24b0411ebaef3ec
SSJ4 <varus552@gmail.com>
2022-11-10 21:57:07 +0700

    Learning kotlin

---
+++ src/main/kotlin/Covariance.kts
@@ -0,0 +1,21 @@
+open class Fruit
+class Banana : Fruit()
+class Orange: Fruit()
+fun receiveFruits(fruits: List<Fruit>) {
+    println("Number of fruits: ${fruits.size}")
+}
+val bananas: List<Banana> = listOf()
+val fruits1: List<Fruit> = listOf()
+receiveFruits(fruits1)
+receiveFruits(bananas) //ERROR: type mismatch
+fun copyFromTo(from: Array<out Fruit>, to: Array<Fruit>) {
+    for (i in 0 until from.size) {
+        to[i] = from[i]
+    }
+}
+val fruitsBasket1 = Array<Fruit>(3) { _ -> Fruit() }
+val fruitsBasket2 = Array<Fruit>(3) { _ -> Fruit() }
+copyFromTo(fruitsBasket1, fruitsBasket2)
+val fruitsBasket = Array<Fruit>(3) { _ -> Fruit() }
+val bananaBasket = Array<Banana>(3) { _ -> Banana() }
+copyFromTo(bananaBasket, fruitsBasket) //ERROR: type mismatch
--- src/main/kotlin/test2.kts
+++ src/main/kotlin/test2.kts
@@ -1,12 +1,16 @@
-fun nickName(name: String?): String? {
-    if (name == "William") {
-        return "Bill"
+import java.lang.RuntimeException
+
+abstract class Book(val name: String)
+class Fiction(name: String) : Book(name)
+class NonFiction(name: String) : Book(name)
+val books: List<Book> = listOf(Fiction("Moby Dick"), NonFiction("Learn to Code"),
+    Fiction("LOTR"))
+
+inline fun <reified T> findFirst(books: List<Book>): T {
+    val selected = books.filter { book -> book is T }
+    if (selected.size == 0) {
+        throw RuntimeException("Not found")
     }
-    if (name != null) {
-        return name.reversed()
-    }
-    return null
+    return selected[0] as T
 }
-println("Nickname for William is ${nickName("William")}")
-println("Nickname for Venkat is ${nickName("Venkat")}")
-println("Nickname for null is ${nickName(null)}")
+println(findFirst<Fiction>(books).name)
