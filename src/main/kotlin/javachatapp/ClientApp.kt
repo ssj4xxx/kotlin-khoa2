eb1784eeb1ac8177c29a4a6bbc1758790a911b47
KHOA121 <varus552@gmail.com>
2022-11-27 17:52:58 +0700

    Learning kotlin chatapp

--- src/main/kotlin/javachatapp/ClientApp.kt
+++ src/main/kotlin/javachatapp/ClientApp.kt
@@ -4,17 +4,14 @@ import java.io.IOException
 import java.net.Socket
 import java.util.*
 
-object ClientApp {
-    @Throws(IOException::class)
-    @JvmStatic
-    fun main(args: Array<String>) {
-        val input = Scanner(System.`in`)
-        val socket = Socket("localhost", 1234)
-        print("Enter your name: ")
-        val name = input.nextLine()
-        val client = Client(socket, name)
-        client.sendNameToServer()
-        client.sender().start()
-        client.receiver().start()
-    }
+
+fun main(args: Array<String>) {
+    val input = Scanner(System.`in`)
+    val socket = Socket("localhost", 1234)
+    print("Enter your name: ")
+    val name = input.nextLine()
+    val client = Client(socket, name)
+    client.sendNameToServer()
+    client.sender().start()
+    client.receiver().start()
 }
